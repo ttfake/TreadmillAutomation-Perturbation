6b1af3d87cba85d133ae8f9aa4d34ea9105b08eb
Tim Fake <tim.t.fake@gmail.com>
2017-06-23 21:10:43 -0400

    * master garbage removal

--- build/CMakeFiles/TreadmillAutomation_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "TreadmillAutomation_automoc.cpp"
-  "CMakeFiles/TreadmillAutomation_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/TreadmillAutomation_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
